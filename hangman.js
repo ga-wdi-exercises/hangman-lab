1fc34b9c8ddd11e82052dbba570f60684fbd8742
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-02-18 18:42:36 -0500

    got the you win if/else statement to work

--- hangman.js
+++ hangman.js
@@ -8,7 +8,7 @@ var concealWord = function (x) {
     var wordWithHints = [];
     for (i = 0; i < x.length; i++) {
         wordWithHints[i] = "-";
-    };
+    }
     return wordWithHints;
 };
 var blankWord = concealWord(splitWord);
@@ -17,45 +17,34 @@ var blankWord = concealWord(splitWord);
 var askForGuess = function() {
   userGuess = prompt("Guess a letter.");
   checkGuess();
-}
+};
 
 var checkGuess = function() {
-
-
-
-
     if (splitWord.includes(userGuess)) {
         for (var i = 0; i < blankWord.length; i++) {
           if (splitWord[i] === userGuess){
-            blankWord[i] = userGuess
+            blankWord[i] = userGuess;
           }
         }
       alert("Correct! Your current progress is " + blankWord);
 
-
-      for(var i = 0; i < splitWord.length; i++) {
-          if (splitWord[i] === blankWord[i]) {
-            alert("You win!");
-          }
-          else {
-            askForGuess();
-          }
-        }
-
-
-
+      if (blankWord.includes("-")) {
+        askForGuess();
+      }
+      else {
+        alert("You win!");
+      }
 
     }
     else {
-        wrongGuesses.push(userGuess)
+        wrongGuesses.push(userGuess);
           if (wrongGuesses.length < 3) {
-            alert("Wrong! Your wrong guesses are " + wrongGuesses)
-            askForGuess()
+            alert("Wrong! Your wrong guesses are " + wrongGuesses);
+            askForGuess();
           }
           else {
             alert("Wrong! You're all out of guesses!  You lose!");
           }
     }
-
-}
+};
 askForGuess();
