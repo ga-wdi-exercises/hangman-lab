8839a2237e268d58a71619db12cbb828a6f2885f
Christine Movius <christine.movius@gmail.com>
2016-02-18 12:05:19 -0500

    added psuedocode for guess letter function.

--- hangman.js
+++ hangman.js
@@ -24,11 +24,27 @@ var game = {
 
   },
 
-  //display a prompt console.log(indexLetters)
-  // var blankWord = prompt("Guess a letter.\n" + console.log(indexLetters[0]) + "\nYou have 6 guesses remaining.");
+  showBlanks: function() {
+    //display a prompt of console.log(indexLetters)
+    blankWord = prompt("Your word is now a secret.\n" + indexLetters[0] + "\n\n Please press 'return' or click 'OK' to continue game.");
+  },
+
+  guessLetter: function() {
+    //display prompt window with blanks. ask user to guess a letter.
+    blankWord = prompt("Guess a letter.\n" + indexLetters[0] + "\nYou have X guesses remaining.");
+
+    //create a guesses function.
+    //start guesses at 6 and decrease by 1 for each incorrect answer.
+    //if prompt input matches a value of secretLetters, replace an indexLetters "_" with matching letter.
+    //if prompt input does not match a value of secretLetters, indexLetters remains the same and guessesRemaining decreases in value by 1. 
+
+  },
+
   playGame: function() {
     game.getSecretWord();
     game.secretIndex();
+    game.showBlanks();
+    game.guessLetter();
   }
 }
 
