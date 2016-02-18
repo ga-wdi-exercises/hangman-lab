f42eb06ef572bde4663e24988580971a911a19d4
Christine Movius <christine.movius@gmail.com>
2016-02-18 10:31:05 -0500

    create prompt for secret word and pushed word into an array of letters. however, letters array seems to be a subarray.

--- hangman.js
+++ hangman.js
@@ -1,1 +1,13 @@
 console.log("welcome to hangman");
+
+secretWord = [];
+
+function getSecretWord() {
+  var promptForWord = prompt("Please enter your secret word.");
+  secretWord.push(promptForWord.split(""));
+}
+
+function playGame() {
+  getSecretWord();
+}
+playGame();
