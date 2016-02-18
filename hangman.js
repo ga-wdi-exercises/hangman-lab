e408c45895f7f1f86982de2068e401c41b2efb39
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-02-18 11:17:09 -0500

    added blankOutWord function and checkGuess function

--- hangman.js
+++ hangman.js
@@ -1,1 +1,34 @@
 console.log("welcome to hangman");
+
+var wordEntered = "elephant";
+var splitWord = wordEntered.split("");
+var userGuess = "e";
+var wrongGuesses = [];
+
+
+
+var blankOutWord = function (splitWord) {
+
+      for (i = 0; i < splitWord.length; i++) {
+          splitWord[i] = "-";
+        }
+    }
+
+
+blankOutWord(splitWord)
+
+alert(splitWord);
+
+
+
+var checkGuess = function() {
+  if (splitWord.includes(userGuess)) {
+    console.log("Correct!")
+  }
+  else {
+    console.log("Wrong!")
+    wrongGuesses.push(userGuess)
+  }
+}
+
+checkGuess();
