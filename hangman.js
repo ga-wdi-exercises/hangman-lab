d984ec9f35c0a5db592bf8c77cdd8affb4c9469b
Andrew Smith-Mui <arsm800@gmail.com>
2016-02-18 10:58:54 -0500

    first crack at pseudocode

--- hangman.js
+++ hangman.js
@@ -1,1 +1,31 @@
+//Test area
 console.log("welcome to hangman");
+alert("Hello!");
+
+
+
+
+
+//Prompt player1 to enter their secret word
+
+//Prompt player2 to guess letter
+  //This window should show dashes that correspond to the number of letters in the secret word.  It should also keep track of the amount of incorrect guesses remaining (begin with 5).
+  //Player2 inputs letter
+
+//If the letter entered by player2 is in the secret word:
+  //Prompt player2 to guess letter
+    //  -replace all the dashes that correspond with that letter with that letter.
+    //  -keep incorrect guess tally at same number
+//  -repeat guess letter prompt.
+
+//Else If the letter entered by player2 is not in the secret word:
+  //Prompt player2 to guess letter
+    //  -all dashes representing secret word should remain dashes
+    //  -reduce incorrect guess tally by 1
+//  -repeat guess letter prompt
+
+//If player2 guesses secret word (all dashes are removed)
+  //alert "Congratulations, player2 wins!"
+
+//If player2 does not guess secret word (guesses remaining = 0)
+  //alert "Sorry, player2 loses"
