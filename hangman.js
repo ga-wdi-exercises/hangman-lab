327b1051ed38e4f806bac5dc9548c7bbb313c5bf
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-18 11:43:41 -0500

    removed player and turn order functions, starting with working enter word function

--- hangman.js
+++ hangman.js
@@ -1,20 +1,43 @@
 var game = {
-  players: [],
   word: [],
   guesses: [],
-  getPlayers: function(){
+  wrongGuesses: [],
+  /*getPlayers: function(){
     while (true){
+    var self = this;
     var playerName = prompt("Welcome to the game! Please enter the player names one at a time. When all players are ready to start the game, type OK.");
-    if(playerName == "OK"){
+    if(playerName == "OK" || playerName == "ok"){
       break;
     }else{
-      game.players.push(playerName);
+      self.players.push(playerName);
       }
     }
+  },
+  goesFirst: function(){
+    game.players.sort(function(a,b){
+      if (Math.random > 0.5) {
+        return 1;
+      }else{
+        return -1;
+      }
+    });
+  },*/
+  chooseWord: function(){
+      var playerWord = prompt("Please choose your secret word!");
+      game.word.push(playerWord);
+      return game.word.length;
+    }
   }
-}
+  /*guessLetter: function(){
+    var playerGuess = prompt("Please guess a letter.")
+    game.players.forEach(player)
+      if (playerGuess == game.word.
+  }*/
 
-game.getPlayers();
+
+
+game.chooseWord();
+console.log(game.word);
 
 /* psuedocode
 
@@ -23,14 +46,20 @@ function getPlayers
 
 function playerTurn
   game.players.sort
+  player[i]
 
 function chooseWord
   prompt
 
 function guessLetter
+  game.players.forEach
+  var playerGuess = prompt("Please guess a letter.")
+  if playerGuess is in the word array, reveal the letter
+    game.guesses.push
+    else if player guess is not in the word array, add playerGuess to the wrongGuesses array
 
 function aNewGame
   game.getPlayers();
-  game.playerTurn();
+  game.goesFirst();
   game.chooseWord
 */
