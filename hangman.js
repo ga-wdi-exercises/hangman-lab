4649dca52b85c9d98b5e62aebdbb41fbdf59b577
Anissa Jones <anissajones10@gmail.com>
2016-02-18 10:36:47 -0500

    Working on psuedocode with classmate Jeff.

--- hangman.js
+++ hangman.js
@@ -1,1 +1,15 @@
 console.log("welcome to hangman");
+
+//Need prompt for word
+//split word into array.
+//print the number of letters to screen OR display progress
+//ask for guess
+//iterate over word/array
+//if letter in word, locate index and replace blank with letter
+//display progress
+//else no change
+//display progress
+// if hyphen exists ask again.
+//else display 'you win'.
+
+//functions include: displayProgress, checkProgress, updateProgress
