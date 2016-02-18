7d59c7bfc0138492e35750ed9f5c04f8728f89cb
Bipin Neupane <icomp2@hotmail.com>
2016-02-18 10:38:50 -0500

    psuedocode psuedocode psuedocode

--- hangman.js
+++ hangman.js
@@ -1,1 +1,4 @@
-console.log("welcome to hangman");
+alert("Welcome to Hang My Best Friend! This is your host, Master Bipin and you get to hang your best friend in this show!!");
+
+prompt ("What is your name, sweetcheeks?!")
+  var name = 
