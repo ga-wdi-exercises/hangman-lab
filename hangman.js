27af60431b82d17070ded2a0e653ce69b4822d85
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-18 10:41:45 -0500

    added basic game array and get player function

--- hangman.js
+++ hangman.js
@@ -1,1 +1,29 @@
-console.log("welcome to hangman");
+var game = {
+  players: [],
+  word: [],
+  guesses: [],
+  getPlayers: function(){
+    console.log("this is how you determine the number of players");
+  }
+}
+
+game.getPlayers();
+
+/* psuedocode
+
+function getPlayers
+  return player
+
+function playerTurn
+  game.players.sort
+
+function chooseWord
+  prompt
+
+function guessLetter
+
+function aNewGame
+  game.getPlayers();
+  game.playerTurn();
+  game.chooseWord
+*/
