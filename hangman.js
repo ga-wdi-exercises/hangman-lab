90d6f00102e6f5f0e900da280d97c2c4c1ed1311
Melissa Miller <lis.d.miller@gmail.com>
2016-02-18 10:34:01 -0500

    Adds pseudocode for program's general logic

--- hangman.js
+++ hangman.js
@@ -1,16 +1,28 @@
 console.log("welcome to hangman");
 
 var game = {
-  word: "",
+  word: [],
+  hidden: "",
   guesses: 5,
 
   getWord: function(){
-    this.word = window.prompt("Enter a word to begin the game.");
+    var temp = window.prompt("Enter a word to begin the game.");
+    this.word = temp.split("");
   },
 
+
+
   playGame: function(){
-    getWord();
+    this.getWord();
+    // display blank string
+    // prompt user for first guesses
+    // if correct, add letter to blank string
+    // else subtract from number of guesses
+    // repeat until...
+
+    // if all letters guessed, display win and word
+    // else if run out of guesses, display lose
   }
-
-
 };
+
+game.playGame();
