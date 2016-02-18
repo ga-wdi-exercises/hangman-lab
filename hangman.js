ca0dccf41881cdbe73e14ff6d6aadbe5cd331588
Bipin Neupane <icomp2@hotmail.com>
2016-02-18 11:37:05 -0500

    split the word and put it in array

--- hangman.js
+++ hangman.js
@@ -4,8 +4,13 @@ var name = prompt ("What is your name, sweetcheeks?!");
 
 var word = prompt("Hi, " + name + "! You're a shitty friend for wanting to hang your best friend but that's NOM Biz. What's your word?");
 
+/* now store that word and mask them so user does not see the word */
+var hideword = word.split("");
+
 var best = prompt("What's your lucky best friend's name?");
 
-/* now store that word and mask them so user does not see the word */
+var mask
 
-var guess = prompt("Alright " + best + "! Start guessing the word " + name + " choose for your death!")
+var guess = alert("Alright " + best + "! Start guessing the word " + name + " choose for your death!")
+
+var guess = prompt("Guess a letter")
