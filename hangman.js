321af640fefac89144e4db15cc3898c23e4c01c9
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-18 13:49:13 -0500

    Add var dashWord = "-"

--- hangman.js
+++ hangman.js
@@ -1,14 +1,13 @@
 console.log("welcome to hangman");
 var word = prompt("Enter a word.");
 console.log("word:" + word);
-var dashWord;
-for (i = 0; i <word.length; i ++) {
+var dashWord = "-";
+for (i = 1; i <word.length; i ++) {
   dashWord += "-";
-  console.log(dashWord);
 }
-// var dashWord = "----";
+// var dashWord = "-----";
 
-prompt ("Word: " + dashWord + " Guess a letter");
+alert("Word: " + dashWord + " Guess a letter");
 
 // {
 //   var str = ("word:");
