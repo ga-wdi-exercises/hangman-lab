8f3d290f3bc5ab102d5bce9dd8b87c21225181da
Charles <charlesthwang@gmail.com>
2016-02-18 16:30:34 -0500

    charleshwang_solution

--- hangman.js
+++ hangman.js
@@ -1,1 +1,33 @@
 console.log("welcome to hangman");
+
+var game = {
+  word: "",
+  //guesses: [],
+  wordLetters: [],
+  maskedLetters: [],
+  playerScore: 0,
+  executionerScore: 0,
+  getWord: function() {
+    this.word = prompt("Enter your secret word.");
+  },
+  splitWord: function() {
+    this.wordLetters = this.word.split("");
+    for (i = 0; i < this.word.length; i++) {
+      this.maskedLetters[i] = "_"
+    }
+    //is there a way to do this using .forEach()?
+    //this.wordLetters.forEach(function(letters) {
+    //  self.maskedLetters.push...?
+    //});
+  },
+  guessLetter: function () {
+    var guess = prompt("Guess a letter.");
+    for (i = 0; i < this.wordLetters; i++) {
+      if (this.wordLetters[i] === guess) {
+        this.maskedLetters[i] = guess;
+        this.playerScore ++;
+      }
+    }
+  }
+
+}
