d51c1fbe6830714d831725f033cf1267b737bce0
Hilary Masland <hilarymasland@gmail.com>
2016-02-18 13:53:33 -0500

    Updated Hangman code

--- hangman.js
+++ hangman.js
@@ -1,20 +1,33 @@
 console.log("Welcome to Hilary's Hangman");
+var guesses
+var incorrectGuesses
 
 word()
 function word() {
-  var word = prompt("What is your secret word?");
-    while (word.length > 1){
-      return mySplit()
+  // var word = prompt("What is your secret word?");
+  while(true){
+    var word = prompt("say word")
+    if (word.length > 1){
+      return word;
 
-
-
-    } if (word.length = 0) {
-      return prompt("Please enter a word")
+    } else {
+      alert("word too short");
     }
-    }
-
-
-
-function mySplit(){
-  var mySplitResult = word.this.split(" ");
+  }
 }
+
+
+    // if (word.length > 1){
+    //   mySplit();
+    //
+    // } else (word.length = 0) {
+    //   return prompt("Please enter a word")
+    // }
+    // }
+
+
+
+
+// function mySplit(){
+//   var mySplitResult = word.this.split(" ");
+// }
