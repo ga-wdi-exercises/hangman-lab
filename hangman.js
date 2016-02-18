8e202e194b9dc57bc2e522728d4451d7a47229be
Hilary Masland <hilarymasland@gmail.com>
2016-02-18 12:14:56 -0500

    hangman progress

--- hangman.js
+++ hangman.js
@@ -3,9 +3,18 @@ console.log("Welcome to Hilary's Hangman");
 word()
 function word() {
   var word = prompt("What is your secret word?");
-    if (word < 1){
-      return console.log("Thanks")
-    } if (word > 0) {
-      return console.log("Wrong")
+    while (word.length > 1){
+      return mySplit()
+
+
+
+    } if (word.length = 0) {
+      return prompt("Please enter a word")
     }
-    };
+    }
+
+
+
+function mySplit(){
+  var mySplitResult = word.this.split(" ");
+}
