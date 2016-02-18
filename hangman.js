adaf9d607961f9130e968340444433e1698d604d
Christine Movius <christine.movius@gmail.com>
2016-02-18 11:48:51 -0500

    created global variables.

--- hangman.js
+++ hangman.js
@@ -1,20 +1,21 @@
 console.log("welcome to hangman");
 var game = {
   //secretLetters: [],
-  indexLetters: ["_"],
+  //indexLetters: ["_"],
 
   getSecretWord: function() {
     //prompt user for secret word
-    var secretWord = prompt("Please enter your secret word.");
+    secretWord = prompt("Please enter your secret word.");
     //split secret word into array of letters
-    var secretLetters = secretWord.split("");
+    secretLetters = secretWord.split("");
     console.log(secretLetters);
 
   },
 
   secretIndex: function() {
+    //for secretLetters.length, create new array ["_", "_"]
     for (var i = 0; i < secretLetters.length; i++) {
-      //var indexLetters = ["_"];
+      indexLetters = ["_"];
       indexLetters.push(); //this creates a subarray of _'s.
       console.log(indexLetters);
       //console.log(indexLetters.length);
@@ -23,7 +24,6 @@ var game = {
 
   },
 
-  //create array of _ that matches length of secretWord
   //display a prompt console.log(indexLetters)
   // var blankWord = prompt("Guess a letter.\n" + console.log(indexLetters[0]) + "\nYou have 6 guesses remaining.");
   playGame: function() {
