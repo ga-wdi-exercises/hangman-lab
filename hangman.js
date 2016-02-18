e053b15eecead73de186aeb380daf71c7eb9b27f
Patrick Moran <patrick_moran@mac.com>
2016-02-18 11:42:22 -0500

    initial commit, draws hangman on misses

--- hangman.js
+++ hangman.js
@@ -1,1 +1,58 @@
 console.log("welcome to hangman");
+var parts = ["( )", "\n\\", "|", "/", "\n | \n", "/ ", "\\"];
+var man = "";
+
+var game = {
+  lettersInWord: [],
+  misses: [],
+  correct: [],
+
+  getANewWord: function () {
+    var word = prompt("Player 1: Give a word to guess:");
+    this.lettersInWord = word.split("");
+    console.log(this.lettersInWord);
+  },
+
+  createCorrect: function () {
+    var self = this;
+    this.lettersInWord.forEach(function () {
+      self.correct.push("_");
+    });
+  },
+
+  getGuesses: function () {
+    var guess;
+    while (this.misses.length < 7) {
+      guess = prompt("Guess:");
+      if (this.lettersInWord.includes(guess)) {
+        var wordIndex = this.lettersInWord.indexOf(guess);
+        this.correct[wordIndex] = guess;
+        console.log("Correct: " + this.correct);
+      } else {
+        this.misses.push(guess);
+        man += parts[this.misses.length - 1];
+        console.log("Misses: " + this.misses);
+        console.log(man);
+      }
+    }
+  },
+
+  // drawMan: function () {
+  //   var self = this;
+  //   this.misses.forEach(function (i) {
+  //     var place = self.misses;
+  //     man += parts[place];
+  //   });
+  //   console.log(man);
+  // },
+
+  playGame: function () {
+    this.getANewWord();
+    this.createCorrect();
+    alert("Ok Player 2: Time to guess");
+    this.getGuesses();
+  }
+
+};
+
+game.playGame();
