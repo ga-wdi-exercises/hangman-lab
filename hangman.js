e47a9252e86305defe77a74070a7b12e9bd61e99
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-18 13:26:12 -0500

    Added word.length to loop.

--- hangman.js
+++ hangman.js
@@ -1,3 +1,19 @@
 console.log("welcome to hangman");
 var word = prompt("Enter a word.");
 console.log("word:", word);
+for (i = 0; i <word.length; i ++) {
+  dashWord += words [i] + "<br>";
+}
+// var dashWord = "----";
+
+prompt ("Word: " + dashWord + " Guess a letter");
+
+// {
+//   var str = ("word:");
+//   var newstr = str.replace ("----");
+//   function replacer (match, "word:", offset,string){
+//     return ["word:"]
+//   };
+//
+// console.log ("word:" + newstr);
+//}
