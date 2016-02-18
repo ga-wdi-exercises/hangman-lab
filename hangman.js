3a3a9db3290b615753f7bde9238ab67671f786ac
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-02-18 11:17:20 -0500

    splits word

--- hangman.js
+++ hangman.js
@@ -1,1 +1,26 @@
-console.log("welcome to hangman");
+
+var word = "";
+var splitWord = [];
+
+var hangman = {
+
+  var word: "",
+  var splitWord = [],
+
+
+
+  getWord: function(){
+    word = prompt("Enter the word you want the other player to guess");
+    splitWord = word.split("");
+    return splitWord;
+  },
+
+  getGuess: function() {
+
+  }
+
+}
+
+
+hangman.getWord();
+console.log(splitWord);
