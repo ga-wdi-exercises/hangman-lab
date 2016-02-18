1f82f07414d0ce4ae11786d4133845f1ccccd41a
Anissa Jones <anissajones10@gmail.com>
2016-02-18 13:46:28 -0500

    Trying to get program to know if guessed letter is in the secret word.

--- hangman.js
+++ hangman.js
@@ -1,28 +1,55 @@
-alert("welcome to hangman");
 
 //Need prompt for word
 //create object
 var hangman = {
-  secretWord: prompt("Player 1 please type your secret word.", "Type word here."),
+  word: "",
+  letter: "",
+  newWord: "",
+  welcome: alert("welcome to hangman"),
 
+  secretWord: function(){
+    this.word = prompt("Player 1 please type your secret word.", "Type word here.").toLowerCase();
+    return this.word;
+  },
 //split word into array.
   splitWord: function(){
-    newWord = this.secretWord.split("");
-    return newWord;
+    this.newWord = this.word.split("");
   },
 
   //print the number of letters to screen OR display dashes.
-  blackSpaces: function(){
+  blankSpaces: function(){
     //for each letter in hangman.splitWord replace with a dash.
-    for(i=0; i<secretWord.length; i++){
+    var displayDashes = [];
+    for(i=0; i<this.word.length; i++){
+      displayDashes.push("-");
+    }
+    return displayDashes;
+  },
 
+  displayProgress: function(){
+    alert(this.blankSpaces().join());
+  },
+
+  //ask for guess
+  guessLetter: function(){
+    this.letter = prompt("Please guess a letter Player2").toLowerCase();
+  },
+
+  //iterate over word/array
+  compareToWord: function(){
+    //compare newWord to letter to guess
+    for (i=0; i<this.newWord.length; i++){
+      if(this.letter === this.newWord[i]){
+        alert("the letter is in word");
+      }
+    console.log(this.newWord);
+    console.log(this.letter);
     }
   }
+
 };
 
-//ask for guess
-//iterate over word/array
-//if letter in word, locate index and replace blank with letter
+//if letter in word, locate index and replace blank with letter. use .find()?
 //display progress
 //else no change
 //display progress
