7eaf8bf2a42f561213e49111653f642cd0ca04f6
YOUR FULL NAME <farishta.haider@gmail.com>
2016-02-18 14:03:22 -0500

    Initial Commit

--- hangman.js
+++ hangman.js
@@ -1,1 +1,19 @@
-console.log("welcome to hangman");
+alert("WELCOME TO HANGMAN\nPress Ok to play!");
+
+var secretWord = prompt("Enter a secret word");
+
+var guess = prompt("Guess a letter");
+
+var guess = [];
+var letters = secretWord.split("");
+var wordLength = secretWord.length;
+var wordSubstring = secretWord.substring();
+var incorrectGuesses = ("guess" - 1);
+var wordBank = ["wombat", "slack", "computer", "function", "commit", "code", "javascript", "github", "outcomes", "atom", "hangman",  "technology", "math", "global", "debugging", "context", "tags", "scope", "closure"];
+
+var guess = prompt("Guess a letter");
+while (guess != secretWord)
+guess +=1;
+if (guess > 9) {
+  prompt("Oh no, you lost!");
+}
