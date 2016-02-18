db5b7ef08822b4ee03eab053caf037fd30653e01
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-18 13:35:19 -0500

    added checks for correct letter, increments incorrect meter by 1

--- hangman.js
+++ hangman.js
@@ -12,32 +12,55 @@ if letter is not contained, add a body part to hangman (strike to the player)
 if it's contained in the original word array, place it in the correct position*/
 
 //once first prompt runs, user word goes into hangmanWord array
+var hangmanWord1 = []
 var hangmanWord = []
+var guessWordHere = [];
+var currentGuess = [];
 var correctGuesses = [];
-var incorrectGuesses = [];
+var incorrectGuesses = 0;
 
 //this function gets the word for the game
-function getWord() {userWord = prompt("Welcome to hangman, please input a word for other user to guess:").toUpperCase();
-  hangmanWord.push(userWord.split(''));
+function getWord() {userWord = prompt("Welcome to hangman, please input a word for other user to guess:");
+    hangmanWord1.push(userWord);
+    //hangmanWord[0].split("");
+  //hangmanWord.push(userWord.split(""));
+
+}
+function splitHMW1() {
+   hangmanWord = hangmanWord1[0].split("")
   console.log(hangmanWord);
 }
 
+/*function updateGuessWordHere()  {
+  for (var i = 0; i < hangmanWord.length; i++)
+  {guessWordHere.push("-")}
+  console.log(hangmanWord.length)
+  console.log(guessWordHere)
+}*/
+
 //this function should get user guesses
-function getUserGuess() {var userGuess = prompt("Please input one letter below").toUpperCase();
-console.log("You guessed: " + userGuess);}
+var getUserGuess = function() {userGuess = prompt("Please input one letter below");
+console.log("You guessed: " + userGuess);
+currentGuess.push(userGuess);
+console.log(currentGuess)}
 
+//this function should evaluate the user guess.
 
-/*  for (var i = 0; i < hangmanWord.length; i++) {
-    console.log(hangmanWord.indexOf(userGuess))
-    if (hangmanWord.indexOf(userGuess) > -1) {
-      console.log("found it")
-    } else {console.log("not there")}}};
-*/
+function evalGuess() {
+  for (var i = 0; i < hangmanWord.length; i++) {
+      if (hangmanWord[i] === userGuess) {
+        return status = "found"
+        break;
+        }
+        else {status = "not found"
+        }
+    }
+};
+function moveGuess() {
+  if (status == "not found") {
+    incorrectGuesses = incorrectGuesses+1;
+  }}
 
-    //if (userGuess === hangmanWord[i]) {console.log("You guessed correctly!")}
-    //else {console.log("WRONG!")}
-    //}
-  //}
 
 
 
@@ -48,4 +71,8 @@ console.log("You guessed: " + userGuess);}
 
 //let's run some functions!
 getWord();
+splitHMW1();
+/*don't worry about this now: updateGuessWordHere();*/
 getUserGuess();
+evalGuess();
+moveGuess();
