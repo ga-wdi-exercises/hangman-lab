a42dc2f813eb68ee745976bc2c2912d869f706b5
Keith Prifte <kprift@gmail.com>
2016-02-18 16:30:58 -0500

    on the right track but adds an infinity loop

--- hangman.js
+++ hangman.js
@@ -1,12 +1,10 @@
 //Welcome Message
 console.log("welcome to hangman");
-
 // Prompts user to get secret word --> UNCOMMENT WHEN DONE <-- secretWord = prompt("Please select a secret word!")
 
   //Hardcoded secretWord, delete when done.
   var secretWord = 'toast'
   console.log('secretWord = ' + secretWord)
-
 
 secretWordHidden = []
 letterGuesses = []
@@ -21,11 +19,29 @@ function spaces(){
   }
 } spaces()
 
+//Loop to start the letter guessing protocol
 
-//1st Prompt asking user2 for  a letter
 letterGuesses.push(prompt('Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter'))
-  console.log(letterGuesses)
-  console.log(secretWordArray.includes(letterGuesses[0]))
+
+
+
+for (i = 0; (i < (secretWord.length)); i++) {
+  //1st Prompt asking user2 to guess a letter
+  if (secretWord.includes(letterGuesses[i]) === true) {
+      for (i = 0; secretWord.length; i++)
+      if (letterGuesses[i] === secretWord[i]) {
+        letterGuesses[i] = secretWordHidden[i]
+      }
+
+    console.log(letterGuesses)
+      letterGuesses[i].replace(secretWordHidden[i])
+      console.log('good job!')
+      console.log('secretWord is ' + secretWord + ' and these letters have been guessed: ' + letterGuesses);
+    } else console.log('loser')
+}
+
+
+correctGuess = letterGuesses.push(prompt('Word: ' + secretWordHidden.join(' ')))
 
 
 //prompt('Word: ' + secretWordHidden.join(' ')+ '\n' + 'You have ' + 'guesses');
