50ff942a66456acaf26cf8d461414c62aa41b945
Bipin Neupane <icomp2@hotmail.com>
2016-02-18 12:03:20 -0500

    Make the word

--- hangman.js
+++ hangman.js
@@ -1,11 +1,19 @@
+/*
+
 var welcome = alert("Welcome to Hang My Best Friend! This is your host, Master Bipin and you get to hang your best friend in this show!!");
 
 var name = prompt ("What is your name, sweetcheeks?!");
 
+*/
+
 var word = prompt("Hi, " + name + "! You're a shitty friend for wanting to hang your best friend but that's NOM Biz. What's your word?");
 
-/* now store that word and mask them so user does not see the word */
-var hideword = word.split("");
+// now store that word and mask them so user does not see the word
+// // var hideword = word.split("");
+// // var rep = hideword.replace (" - ")
+
+var hideword  = word.replace(/./g, " - ")
+
 
 var best = prompt("What's your lucky best friend's name?");
 
@@ -13,4 +21,4 @@ var mask
 
 var guess = alert("Alright " + best + "! Start guessing the word " + name + " choose for your death!")
 
-var guess = prompt("Guess a letter")
+var guess = prompt( hideword + " Guess a letter")
