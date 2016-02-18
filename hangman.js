aa1b3c82131ade276dff32e810b735fb4a54e731
Christine Movius <christine.movius@gmail.com>
2016-02-18 11:26:48 -0500

    created indexLetters array with blank spaces matching length of secretWord, but again creating a subarray.

--- hangman.js
+++ hangman.js
@@ -2,13 +2,25 @@ console.log("welcome to hangman");
 
 function getSecretWord() {
   //prompt user for secret word
-  var promptForWord = prompt("Please enter your secret word.");
+  var secretWord = prompt("Please enter your secret word.");
   //split secret word into array of letters
-  var secretWord = promptForWord.split("");
-  console.log(secretWord);
+  var secretLetters = secretWord.split("");
+  console.log(secretLetters);
+
   //create array of _ that matches length of secretWord
-  
+  for (var i = 0; i < secretLetters.length; i++) {
+    var indexLetters = ["_"];
+    indexLetters.push(); //this creates a subarray of _'s.
+    console.log(indexLetters);
+    //console.log(indexLetters.length);
+    //console.log(secretLetters);
+
+    //display a prompt console.log(indexLetters)
+    var blankWord = prompt("Guess a letter.\n" + console.log(indexLetters[0]) + "\nYou have 6 guesses remaining.");
+  }
 }
+
+
 
 function playGame() {
   getSecretWord();
