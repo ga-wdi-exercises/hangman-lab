aa12171456268fbcf49e5d713d2cfe5211578e21
Christine Movius <christine.movius@gmail.com>
2016-02-18 11:41:43 -0500

    made game an object to separate functions.

--- hangman.js
+++ hangman.js
@@ -1,28 +1,35 @@
 console.log("welcome to hangman");
+var game = {
+  //secretLetters: [],
+  indexLetters: ["_"],
 
-function getSecretWord() {
-  //prompt user for secret word
-  var secretWord = prompt("Please enter your secret word.");
-  //split secret word into array of letters
-  var secretLetters = secretWord.split("");
-  console.log(secretLetters);
+  getSecretWord: function() {
+    //prompt user for secret word
+    var secretWord = prompt("Please enter your secret word.");
+    //split secret word into array of letters
+    var secretLetters = secretWord.split("");
+    console.log(secretLetters);
+
+  },
+
+  secretIndex: function() {
+    for (var i = 0; i < secretLetters.length; i++) {
+      //var indexLetters = ["_"];
+      indexLetters.push(); //this creates a subarray of _'s.
+      console.log(indexLetters);
+      //console.log(indexLetters.length);
+      //console.log(secretLetters);
+    }
+
+  },
 
   //create array of _ that matches length of secretWord
-  for (var i = 0; i < secretLetters.length; i++) {
-    var indexLetters = ["_"];
-    indexLetters.push(); //this creates a subarray of _'s.
-    console.log(indexLetters);
-    //console.log(indexLetters.length);
-    //console.log(secretLetters);
-
-    //display a prompt console.log(indexLetters)
-    var blankWord = prompt("Guess a letter.\n" + console.log(indexLetters[0]) + "\nYou have 6 guesses remaining.");
+  //display a prompt console.log(indexLetters)
+  // var blankWord = prompt("Guess a letter.\n" + console.log(indexLetters[0]) + "\nYou have 6 guesses remaining.");
+  playGame: function() {
+    game.getSecretWord();
+    game.secretIndex();
   }
 }
 
-
-
-function playGame() {
-  getSecretWord();
-}
-playGame();
+game.playGame();
