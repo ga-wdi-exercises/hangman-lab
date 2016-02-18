4c69f27cdc4217ea2b5c3cdd93861f64bbd14b2e
Maureen VOGEL <maureenvogel@Maureen-VOGELs-MacBook-Pro.local>
2016-02-18 15:03:16 -0500

    Made the game work, now I'm going to set the number of guesses to five

--- hangman.js
+++ hangman.js
@@ -1,22 +1,50 @@
 var split;
 var word;
+var guess;
 // get user word 
 var getWord=function(){
 	word=prompt("Choose a word.").toLowerCase();
-	splitWord();
 	return word;
 };
 
-var splitWord=function(){
-	split=word.split("");
+var splitWord=function(wordToSplit){ 
+	split=wordToSplit.split("");
+	word=word.split("");
 	
-	for (i=0; i<split.length; i++){
+	console.log("split:", split);
+	
+	for (var i=0; i<split.length; i++){
 		split[i] = "_";
-		return split;
 	}
+	
+	return split;
+};
+
+var guessWord=function(){
+
+	guess=prompt("Please guess a letter to fill in the blank: " + split).toLowerCase();
+	
+
+	for (var i=0; i<word.length; i++){
+		if(word[i]===guess){
+		  split[i] = guess;
+		}
+    }
+    if (!word.includes(guess)) {
+        alert("That letter is not in the word. Try again.");
+    }
+    
+    if (split.includes("_")){
+        guessWord();
+    } else {
+        alert("Congratulations you've guessed the word. The word was " + word.join(""));
+    }
+
+	
 };
 
 
 
-getWord();
-console.log(split);
+hangmanWord = getWord();
+console.log(splitWord(hangmanWord));
+console.log(guessWord());
