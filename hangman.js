66bff369f2a590fb090661b75ea91508e02452af
Maureen VOGEL <maureenvogel@Maureen-VOGELs-MacBook-Pro.local>
2016-02-18 11:17:49 -0500

    I've converted the words into underscores

--- hangman.js
+++ hangman.js
@@ -1,1 +1,22 @@
-console.log("welcome to hangman");
+var split;
+var word;
+// get user word 
+var getWord=function(){
+	word=prompt("Choose a word.").toLowerCase();
+	splitWord();
+	return word;
+};
+
+var splitWord=function(){
+	split=word.split("");
+	
+	for (i=0; i<split.length; i++){
+		split[i] = "_";
+		return split;
+	}
+};
+
+
+
+getWord();
+console.log(split);
