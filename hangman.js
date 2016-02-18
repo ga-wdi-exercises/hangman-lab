119e3ed7c4b7c5d3972834b0edd225378b44a56f
Toni Langley <chibia2@gmail.com>
2016-02-18 15:30:50 -0500

    functioning game based on input from user with option to play again

--- hangman.js
+++ hangman.js
@@ -1,56 +1,78 @@
-console.log("welcome to hangman");
+var answer;
+game();
+function game(){
+  console.log("welcome to hangman");
 
-//LATER: ask for number of players
-//LATER: get player names| can use .split()
+  //!!!LATER: ask for number of players
+  //!!!LATER: get player names| can use .split()
 
-//****players assigned game roles
-var inCharge;
-var guessers;
-var correct = [];
-var incorrect = [];
-
-//***the player inCharge provides answer
-//the spaces from answer are removed, the remaining characters are joined together, displayed in an array output
-var answer = prompt("Choose a word, phrase, or sentence for the game.").replace(/ /g, "").split('');
-
-
-while(answer){
-guessing();
-  function guessing(){
-
-  //***have guessers...guess answer
-  var guess = prompt("What letter or number will you choose?");
+  //****players assigned game roles
+  var inCharge;
+  var guessers;
+  var correct = [];
+  var incorrect = [];
   var count = 0;
-  //var correct = [];
-  //var incorrect = [];
 
-  if( !(answer.includes(guess))){
-    console.log("Nope, sorry.");
-    incorrect.push(guess);
-    count++;
-    console.log(count);
+  //***the player inCharge provides answer
+  //the spaces from answer are removed, the remaining characters are joined together, displayed in an array output
+
+  answer = beg = prompt("Choose a word, phrase, or sentence for the game.");
+  //answer.replace(/ /g, "").split('');
+
+  while(answer){
     guessing();
-    //move on to next player
-    //if count reaches #, game over
-  }
-  else if(answer.includes(guess)){
-    console.log("Correct! " + guess);
-    //answer.remove(guess);
-    correct.push(guess);
-    console.log(correct);
+    function guessing(){
 
-    if(correct.length == answer.length){
-      answer = false;
-    }
-    else{
+      //***have guessers...guess answer
+      var guess = prompt("What letter or number will you choose?");
+
+      //wrong answer
+      if( !(answer.includes(guess)) ){
+        console.log("Nope, sorry.");
+        incorrect.push(guess);
+        count++;
+        console.log(count);
+
+        //game over
+        if(count == 3){
+          console.log("Game Over");
+          answer=false;
+        }
+        else{
           guessing();
+        }
+        //move on to next player
+      }
+
+      //correct answer
+      else if(answer.includes(guess)){
+        console.log("Correct! " + guess);
+        correct.push(guess);
+        console.log(correct);
+
+        //play again?
+        if(correct.length == answer.length){
+          console.log("You got it! \n The answer was " + beg);
+          oneMoreTime();
+        }
+      }
     }
-    //answer=false;
-    //repeat question so this should be a function (CALL IT HERE!!!)
+    //console.log("mega: " + correct);
   }
+}
 
-  console.log("mega: " + correct);
-
+//!!!bring to top
+function oneMoreTime(){
+  var again = prompt("Would you like to play again? (Y/N)");
+  if(again == "Y"){
+    game();
   }
-
+  else if(again == "N"){
+    console.log("Game Over");
+    answer = false;
+  }
+  else{
+    console.log("Invalid Entry: Please try again");
+    oneMoreTime();
+  }
 }
