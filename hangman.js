ca22a2008efd94d08724e23040ffa4a5a3fd8354
Melissa Miller <lis.d.miller@gmail.com>
2016-02-18 10:24:31 -0500

    Adds getWord and playGame methods

--- hangman.js
+++ hangman.js
@@ -1,1 +1,16 @@
 console.log("welcome to hangman");
+
+var game = {
+  word: "",
+  guesses: 5,
+
+  getWord: function(){
+    this.word = window.prompt("Enter a word to begin the game.");
+  },
+
+  playGame: function(){
+    getWord();
+  }
+
+
+};
