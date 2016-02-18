c77bf2a5f999e6ea91ec4399d1d4b7be5653423d
Bipin Neupane <icomp2@hotmail.com>
2016-02-18 10:54:16 -0500

    welcome the player

--- hangman.js
+++ hangman.js
@@ -1,4 +1,4 @@
-alert("Welcome to Hang My Best Friend! This is your host, Master Bipin and you get to hang your best friend in this show!!");
+var welcome = alert("Welcome to Hang My Best Friend! This is your host, Master Bipin and you get to hang your best friend in this show!!");
 
-prompt ("What is your name, sweetcheeks?!")
-  var name = 
+var name = prompt ("What is your name, sweetcheeks?!");
+alert("Hi, " + name + "! You're a shitty friend for wanting to hang your best friend but that's NOM Biz. What's your word?");
