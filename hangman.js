b857413df90c3632a90b44a8fcd6552f1401048e
Bipin Neupane <icomp2@hotmail.com>
2016-02-18 11:09:52 -0500

    set up players

--- hangman.js
+++ hangman.js
@@ -1,4 +1,11 @@
 var welcome = alert("Welcome to Hang My Best Friend! This is your host, Master Bipin and you get to hang your best friend in this show!!");
 
 var name = prompt ("What is your name, sweetcheeks?!");
-alert("Hi, " + name + "! You're a shitty friend for wanting to hang your best friend but that's NOM Biz. What's your word?");
+
+var word = prompt("Hi, " + name + "! You're a shitty friend for wanting to hang your best friend but that's NOM Biz. What's your word?");
+
+var best = prompt("What's your lucky best friend's name?");
+
+/* now store that word and mask them so user does not see the word */
+
+var guess = prompt("Alright " + best + "! Start guessing the word " + name + " choose for your death!")
