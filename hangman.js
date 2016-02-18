958928648a55b6d8374e35d67c794d3a7e2a9303
Toni Langley <chibia2@gmail.com>
2016-02-18 12:36:23 -0500

    loops through answer based on guess, stops when lengths of answer and guess equal

--- hangman.js
+++ hangman.js
@@ -6,29 +6,51 @@ console.log("welcome to hangman");
 //****players assigned game roles
 var inCharge;
 var guessers;
+var correct = [];
+var incorrect = [];
 
 //***the player inCharge provides answer
 //the spaces from answer are removed, the remaining characters are joined together, displayed in an array output
 var answer = prompt("Choose a word, phrase, or sentence for the game.").replace(/ /g, "").split('');
 
-guessing();
-function guessing(){
+
 while(answer){
+guessing();
+  function guessing(){
 
   //***have guessers...guess answer
   var guess = prompt("What letter or number will you choose?");
   var count = 0;
+  //var correct = [];
+  //var incorrect = [];
 
   if( !(answer.includes(guess))){
     console.log("Nope, sorry.");
+    incorrect.push(guess);
     count++;
+    console.log(count);
+    guessing();
     //move on to next player
+    //if count reaches #, game over
   }
   else if(answer.includes(guess)){
-    console.log("Correct!");
-    answer=false;
+    console.log("Correct! " + guess);
+    //answer.remove(guess);
+    correct.push(guess);
+    console.log(correct);
+
+    if(correct.length == answer.length){
+      answer = false;
+    }
+    else{
+          guessing();
+    }
+    //answer=false;
     //repeat question so this should be a function (CALL IT HERE!!!)
+  }
+
+  console.log("mega: " + correct);
 
   }
-}
+
 }
