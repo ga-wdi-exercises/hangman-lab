85b5d47ff562a2707b082657123f86ba8a00ad4b
Hilary Masland <hilarymasland@gmail.com>
2016-02-18 15:40:32 -0500

    Further editing

--- hangman.js
+++ hangman.js
@@ -4,24 +4,34 @@ console.log("Welcome to Hilary's Hangman");
 word()
 function word() {
   // var word = prompt("What is your secret word?");
-  while(true){
-    var word = prompt("What is your secret word?")
-    if (word.length > 1){
-      return word;
-    }else{
-      
+
+    var word = prompt("What is your secret word?");
+    secret = word.length;
+    if (secret > 1){
+      GameWord = word.split("");
+      LetterHolder = [];
+      for (var i = 0; i < GameWord.length; i++){
+        LetterHolder[i]="-";
+      }
     }
-  }
+    else{
+       console.log("x");
+
+    }
 }
-//
+
+function Game() {
+  
+}
+
 // }
 // function guesses(word){
 // var guess = prompt("Guess a letter");
 //   while(true){
 //     if (guess.length = 1){
 //       return guess;
-//     }
-//   }
+// //     }
+// //   }
 // };
 
 
