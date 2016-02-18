2114da8546f18bdd3864139d7674ef4f1f60f365
Hilary Masland <hilarymasland@gmail.com>
2016-02-18 11:20:45 -0500

    Initial commit for Hangman

--- hangman.js
+++ hangman.js
@@ -1,1 +1,11 @@
-console.log("welcome to hangman");
+console.log("Welcome to Hilary's Hangman");
+
+word()
+function word() {
+  var word = prompt("What is your secret word?");
+    if (word < 1){
+      return console.log("Thanks")
+    } if (word > 0) {
+      return console.log("Wrong")
+    }
+    };
