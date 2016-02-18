22e36ebb818caeb9eadb59e795174bb27f6d139d
Christine Movius <christine.movius@gmail.com>
2016-02-18 10:45:52 -0500

    word prompt into array of letters.

--- hangman.js
+++ hangman.js
@@ -1,10 +1,13 @@
 console.log("welcome to hangman");
 
-secretWord = [];
-
 function getSecretWord() {
+  //prompt user for secret word
   var promptForWord = prompt("Please enter your secret word.");
-  secretWord.push(promptForWord.split(""));
+  //split secret word into array of letters
+  var secretWord = promptForWord.split("");
+  console.log(secretWord);
+  //create array of _ that matches length of secretWord
+  
 }
 
 function playGame() {
