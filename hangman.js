6c6861dc4155b15f4b92f792198aead9a07efb4a
Bipin Neupane <icomp2@hotmail.com>
2016-02-18 16:08:41 -0500

    working app, but stuck on the loop if guessed incorrectly

--- hangman.js
+++ hangman.js
@@ -1,20 +1,19 @@
-/*
 
-var welcome = alert("Welcome to Hang My Best Friend! This is your host, Master Bipin and you get to hang your best friend in this show!!");
+
+var welcome = alert("Welcome to Hang My Best Friend! This is your host, Master Bipin and you get to hang your best friend in this show. Let's Begin!!");
 
 var name = prompt ("What is your name?");
 
-*/
+
 
 var word = prompt("Hi, " + name + "! You're a shitty friend for wanting to hang your best friend but that's NOM Biz. What's your word?");
 
-// put that word into an array
-//var hiddenword = word.split("");
+var hiddenword = word.split("");
 
-var mask  = word.replace(/./g, "-");
+var mask  = word.replace(/./g, " _ ");
 
 
-//var best = prompt("What's your lucky best friend's name?");
+var best = prompt(name +", which friend do you want to hang today?");
 
 
 
@@ -24,7 +23,7 @@ var badguess = []
 
 while (goodguess.length < word.length) {
 
-  var guessedLetter = prompt("Alright! Start guessing the word " + name + " chose for your death!")
+  var guessedLetter = prompt(mask + " We have a word! " + best + " Start guessing the word " + name + " chose for your death!")
 
   var place = word.indexOf(guessedLetter);
   if (place === -1 ) {
@@ -33,7 +32,7 @@ while (goodguess.length < word.length) {
     badguess.push(guessedLetter);
 
     while (badguess.length > word.length) {
-      alert("You just hung your friend!");
+      alert(name + ", you're terrible friend and you just hung your best friend " + best + " !");
     break;
   }
 
