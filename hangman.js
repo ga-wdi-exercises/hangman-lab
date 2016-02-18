2bb56662b292d83f9dbf5d1f43aacca75a1bbf92
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-02-18 12:31:17 -0500

    first draft of checkGuess

--- hangman.js
+++ hangman.js
@@ -1,13 +1,13 @@
 
-
-
 var hangman = {
   word: "",
   splitWord: [],
   displayWord: [],
+  currentGuess: [],
+  spliceNum: [],
 
   getWord: function(){
-    this.word = prompt("Enter the word you want the other player to guess");
+    this.word = prompt("Enter the word you want the other player to guess").toLowerCase();
     this.splitWord = this.word.split("");
   },
 
@@ -17,13 +17,30 @@ var hangman = {
     }
   },
 
-
   getGuess: function() {
+    this.currentGuess = prompt("Enter a letter").toLowerCase();
+  },
 
+  checkGuess: function() {
+    if (this.currentGuess in this.splitWord) {
+      var index = this.splitWord.indexOf(this.currentGuess);
+      this.splitWord[index] = this.currentGuess;
+      if ("-" in this.splitWord) {
+        getGuess();
+        }
+        else {
+          alert("You win!");
+        }}
+    else {
+        alert("That letter not in the secret word.")
+        getGuess();
+      }
+    }
   }
 
-}
 
 
 hangman.getWord();
 hangman.displayProgress();
+hangman.getGuess();
+hangman.checkGuess();
