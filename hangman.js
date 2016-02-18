c63ba3683f1475eccedca3db3499b93689209bd7
Keith Prifte <kprift@gmail.com>
2016-02-18 13:47:25 -0500

    adds prompt to get secretWord and shows user # of letters in secretWord

--- hangman.js
+++ hangman.js
@@ -1,1 +1,31 @@
+//Welcome Message
 console.log("welcome to hangman");
+
+// Prompts user to get secret word --> UNCOMMENT WHEN DONE <-- secretWord = prompt("Please select a secret word!")
+
+  //Hardcoded secretWord, delete when done.
+  var secretWord = 'toast'
+  console.log('secretWord = ' + secretWord)
+
+
+secretWordHidden = []
+letterGuesses = []
+
+secretWordArray = secretWord.split('');
+console.log(secretWordArray);
+
+// Function to show user the amount of letters in the secret word
+function spaces(){
+  for (i = 0; i < secretWord.length; i++) {
+    secretWordHidden.push('_');
+  }
+} spaces()
+
+
+//1st Prompt asking user2 for  a letter
+letterGuesses.push(prompt('Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter'))
+  console.log(letterGuesses)
+  console.log(secretWordArray.includes(letterGuesses[0]))
+
+
+//prompt('Word: ' + secretWordHidden.join(' ')+ '\n' + 'You have ' + 'guesses');
