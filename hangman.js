7473b306e06e17c1452c513513ab600b8cd06755
Vijender Singh Bajwa <vbajwamd@gmail.com>
2016-02-18 14:37:48 -0500

    sofar

--- hangman.js
+++ hangman.js
@@ -1,1 +1,19 @@
 console.log("welcome to hangman");
+
+var wordBank= ['monday', 'ennui', 'legit', 'beer', 'butt', 'llama', 'wombat'];
+var answer;
+var gameWord;
+var guesses= [];
+
+function chooseWord(){
+  var wordIndex = Math.floor(Math.random()* wordBank.length);
+  var gameWord = wordBank[wordIndex];
+  return gameWord;
+}
+
+gameWord = chooseWord();
+  for(guess=0;guess<10;guess++){
+    answer = prompt("Enter A Letter");
+    guesses.push(answer);
+    console.log(guesses);
+  }
