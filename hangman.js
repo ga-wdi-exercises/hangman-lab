e4e9d1655630fcfb22d87ec0c6bfd898df328ff7
Woo Yun <hh747hh@gmail.com>
2016-02-18 23:58:08 -0500

    fist commit

--- hangman.js
+++ hangman.js
@@ -1,1 +1,39 @@
 console.log("welcome to hangman");
+var word;
+var guesses = [];
+var incorrectGuesses;
+
+var word = prompt ("Please enter your secret word.");
+var  word = word.split("");
+
+  var getWord = prompt("Guess one letter.")
+  if(word.includes(getWord){
+  alert("You entered " + getWord);
+}
+
+
+
+ //Players take turns guessing a letter from "letters" array
+   // if letter is stored in "word" array, remove from "letters", show in puzzle
+   // if user selects a removed letter, display prompt
+
+ // function attempts () {
+ //     while (userattempts < 6) {
+ //       var guess = prompt ("Please pick a letter:")
+ //       userattempts ++;
+ //         if (this.word.includes(guess)) {
+ //           alert ("That's correct.");
+ //         } else {
+ //            alert("That's incorrect.")
+ //       }
+ //     }
+ //   }
+ //
+ // //Each player gets 5 attempts
+ //
+ //
+ //
+ //
+ // /* users(); */
+ // pickAWord();
+ // attempts ();
