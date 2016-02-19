3957681ad42b864a4be59c96ca3dafb101f67322
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-02-19 14:11:02 -0500

    Initial commit

---
+++ caitlinSolution.js
@@ -0,0 +1,13 @@
+var game = {
+  // Choose word
+
+  // Split word into an array
+
+  // Take letter input
+
+  // Store letter input in guess array
+
+  // Check guesses array with letters array
+
+  // End game if 5 wrong guesses or guesses.length === word.length
+}
