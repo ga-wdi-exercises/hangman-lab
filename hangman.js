7330c33f56ad4f766b9433b3d2c281b92f994632
Keith Prifte <kprift@gmail.com>
2016-02-18 19:24:42 -0500

    adds comments, need to revise the loops because they are messing the IF statement logic

--- hangman.js
+++ hangman.js
@@ -20,32 +20,35 @@ function spaces(){
 } spaces()
 
 //Loop to start the letter guessing protocol
-
-
 for (i = 0; (i < (secretWord.length)); i++) {
-  console.log(i + 'loop')
-
-  firstGuessPrompt()
-
-
+  //Executes prompt
+  letterGuesses.push(prompt('Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter.  Guesses left: ' + ((secretWord.length + 1)- i)))
+  console.log(i + ' time running ; letterGuessed = ' + letterGuesses[i] + 'letterGuesses = ' + letterGuesses)
+  //2nd loop to evaluate if the guess was correct
   if (secretWord.includes(letterGuesses[i]) === true) {
-
+      //If the letter guessed is in the secretWord, the 1st IF statement evulates here to the 2nd loop
       for (j = 0; j < secretWord.length; j++) {
-
+      // 2nd loop is needed to check the guess against ALL letters of the secretWord
       if (letterGuesses[i] === secretWord[j]) {
+        //IF the guess is the same as ANY letter in the secretWord, we then overwrite the secretWordHidden array which is all underscores ("_")
         secretWordHidden[j] = letterGuesses[i]
-      } else console.log('i = ' + i + ', j = ' + j + ', letterGuesses[i] = ' + letterGuesses[i] + ', secretWord[j] = ' + secretWord[j] + ', and secretWordHidden = ' + secretWordHidden);
-
-    }  letterGuesses.push(prompt('CORRECT!!!' + '\n' + 'Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter.  Guesses left: ' + ((secretWord.length + 1)- i)));
-
+        detailedCorrectConsoleMessage()
+        console.log('detailedCorrectConsoleMessage')
+        //end of nested IF statement
+      } else detailedWrongConsoleMessage()
+        console.log('detailedWrongConsoleMessage');
+  //1st loop evulates here once if runs though the nested if and 2nd for loop
+  }  correctMessage()
+    console.log(i + 'correctMessage()');
   } else if (secretWord.includes(letterGuesses[i]) !== true) {
-    incorrectMessage();
+    incorrectMessage()
+    console.log(i + ' secretWord.includes(letterGuesses[i]) !== true');
   } else if (secretWordHidden === secretWord) {
-
     alert('You Win!' + '\n' + 'Word:  ' + secretWordHidden.join(' '))
   }
 }
 
+//End of IF statement.  Beginning of Message functions:
 function firstGuessPrompt() {
   letterGuesses.push(prompt('Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter.  Guesses left: ' + ((secretWord.length + 1)- i)))
 }
@@ -53,6 +56,20 @@ function firstGuessPrompt() {
 function incorrectMessage() {
   letterGuesses.push(prompt('INCORRECT!!!' + '\n' + 'Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter.  Guesses left: ' + ((secretWord.length + 3)- i)))
 }
+
+function detailedCorrectConsoleMessage() {
+  console.log('Correct.  i = ' + i + ', j = ' + j + ', letterGuesses[i] = ' + letterGuesses[i] + ', secretWord[j] = ' + secretWord[j] + ', and secretWordHidden = ' + secretWordHidden)
+}
+
+function detailedWrongConsoleMessage() {
+  console.log('Wrong.  i = ' + i + ', j = ' + j + ', letterGuesses[i] = ' + letterGuesses[i] + ', secretWord[j] = ' + secretWord[j] + ', and secretWordHidden = ' + secretWordHidden)
+}
+
+function correctMessage() {
+  letterGuesses.push(prompt('CORRECT!!!' + '\n' + 'Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter.  Guesses left: ' + ((secretWord.length + 1)- i)));
+}
+
+
 //correctGuess = letterGuesses.push(prompt('Word: ' + secretWordHidden.join(' ')))
 
 
